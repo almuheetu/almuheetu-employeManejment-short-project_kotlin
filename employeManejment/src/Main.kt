3ad0459d5ec0f45c2ae33befbdaec57ebb416b6c
Almuheetu Shihab <almuheetu@gmail.com>
2024-05-20 10:57:43 +0600

    name,age and base salary print

--- employeManejment/src/Main.kt
+++ employeManejment/src/Main.kt
@@ -7,20 +7,23 @@ fun main() {
     employees.add(Marketing(name = "Rahim", age = 30, location = "Rajshahi", baseSalary = 15000))
     employees.add(Marketing(name = "jarin", age = 30, location = "feny", baseSalary = 15000))
     employees.add(Marketing(name = "harim", age = 30, location = "citagong", baseSalary = 15000))
+
     employees.add(Accountant(name = "Rahim", age = 25, location = "Chittagong", baseSalary = 30000))
     employees.add(Accountant(name = "Rahim", age = 25, location = "Chittagong", baseSalary = 30000))
     employees.add(Accountant(name = "Rahim", age = 25, location = "Chittagong", baseSalary = 30000))
     employees.add(Accountant(name = "Rahim", age = 25, location = "Chittagong", baseSalary = 30000))
+
     employees.add(Manager(name = "Samir", age = 40, location = "Sylhet", baseSalary = 55000))
-    employees.add(Manager(name = "Samir", age = 40, location = "Sylhet", baseSalary = 55000))
-    employees.add(Manager(name = "Samir", age = 40, location = "Sylhet", baseSalary = 55000))
-    employees.add(Manager(name = "Samir", age = 40, location = "Sylhet", baseSalary = 55000))
-    employees.add(Manager(name = "Samir", age = 40, location = "Sylhet", baseSalary = 55000))
-    employees.add(Sales(name = "Rafiq", age = 28, location = "Barisal", baseSalary = 22000))
-    employees.add(Sales(name = "Rafiq", age = 28, location = "Barisal", baseSalary = 22000))
-    employees.add(Sales(name = "Rafiq", age = 28, location = "Barisal", baseSalary = 22000))
-    employees.add(Sales(name = "Rafiq", age = 28, location = "Barisal", baseSalary = 22000))
-    employees.add(Sales(name = "Rafiq", age = 28, location = "Barisal", baseSalary = 22000))
+    employees.add(Manager(name = "samiya", age = 40, location = "Sylhet", baseSalary = 51000))
+    employees.add(Manager(name = "jannat", age = 40, location = "Sylhet", baseSalary = 45000))
+    employees.add(Manager(name = "Rabeya", age = 40, location = "Sylhet", baseSalary = 54000))
+    employees.add(Manager(name = "Basori", age = 40, location = "Sylhet", baseSalary = 69000))
+
+    employees.add(Sales(name = "Rabbi", age = 28, location = "Feni", baseSalary = 22000))
+    employees.add(Sales(name = "Apple", age = 28, location = "naogaon", baseSalary = 22000))
+    employees.add(Sales(name = "Sazzad", age = 28, location = "Barisal", baseSalary = 22000))
+    employees.add(Sales(name = "Mahfuz", age = 28, location = "Barisal", baseSalary = 22000))
+    employees.add(Sales(name = "Sakib", age = 28, location = "kulna", baseSalary = 22000))
 
     var marketingCount = 0
     var accountantCount = 0
@@ -39,8 +42,24 @@ fun main() {
         }
     }
 
-  println("Total emoployee ${employees.size}")
-    println("Marketing $marketingCount")
+    println("Total emoployees : ${employees.size}")
+    println("Marketing : $marketingCount")
+    println("Accountant : $accountantCount")
+    println("Maneger : $managerCount")
+    println("Sales : $salesCount")
+
+for (employee in employees){
+
+    val employName = employee.name
+    println("Name : $employName")
+
+    val employAge = employee.age
+    println("Age : $employAge")
+
+
+    val baseSalary = employee.salary()
+    println("Base salary : $baseSalary")
+}
 
 
 }
--- employeManejment/src/Sales.kt
+++ employeManejment/src/Sales.kt
@@ -4,12 +4,12 @@ class Sales(name: String, age: Int, location: String? = null, private val baseSa
     override fun salary(): Int = baseSalary
 
     override fun calculateBonus(salary: Int): Int {
-        return if (10000 <= 20000) {
-            return 1000
-        } else if (20001 <= 50000) {
-            return 2000
-        } else if (50001 <= 80000) {
-            return  3000
+        return if (salary in 10000..20000) {
+            1000
+        } else if (salary in 20001..50000) {
+            2000
+        } else if (salary in 50001..80000) {
+            3000
         } else {
             0
         }
