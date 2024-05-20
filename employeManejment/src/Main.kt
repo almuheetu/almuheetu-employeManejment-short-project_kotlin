4620fc297bfe7cab1e3e7072624fa8cf10f95cdd
Almuheetu Shihab <almuheetu@gmail.com>
2024-05-20 11:33:01 +0600

    employe name and age fixing

--- employeManejment/src/Main.kt
+++ employeManejment/src/Main.kt
@@ -3,27 +3,30 @@ fun main() {
 
 
 
-    employees.add(Marketing(name = "Karim", age = 30, location = "Dhaka", baseSalary = 15000))
-    employees.add(Marketing(name = "Rahim", age = 30, location = "Rajshahi", baseSalary = 15000))
-    employees.add(Marketing(name = "jarin", age = 30, location = "feny", baseSalary = 15000))
-    employees.add(Marketing(name = "harim", age = 30, location = "citagong", baseSalary = 15000))
+    val marketingEmployees =Marketing(name = "Karim", age = 30, location = "Dhaka", baseSalary = 15000)
+    employees.add(marketingEmployees)
 
-    employees.add(Accountant(name = "Rahim", age = 25, location = "Chittagong", baseSalary = 30000))
-    employees.add(Accountant(name = "Rahim", age = 25, location = "Chittagong", baseSalary = 30000))
-    employees.add(Accountant(name = "Rahim", age = 25, location = "Chittagong", baseSalary = 30000))
-    employees.add(Accountant(name = "Rahim", age = 25, location = "Chittagong", baseSalary = 30000))
 
-    employees.add(Manager(name = "Samir", age = 40, location = "Sylhet", baseSalary = 55000))
-    employees.add(Manager(name = "samiya", age = 40, location = "Sylhet", baseSalary = 51000))
-    employees.add(Manager(name = "jannat", age = 40, location = "Sylhet", baseSalary = 45000))
-    employees.add(Manager(name = "Rabeya", age = 40, location = "Sylhet", baseSalary = 54000))
-    employees.add(Manager(name = "Basori", age = 40, location = "Sylhet", baseSalary = 69000))
+    employees.add(Marketing(name = "Rahim", age = 31, location = "Rajshahi", baseSalary = 15000))
+    employees.add(Marketing(name = "jarin", age = 32, location = "feny", baseSalary = 15000))
+    employees.add(Marketing(name = "harim", age = 33, location = "citagong", baseSalary = 15000))
 
-    employees.add(Sales(name = "Rabbi", age = 28, location = "Feni", baseSalary = 22000))
-    employees.add(Sales(name = "Apple", age = 28, location = "naogaon", baseSalary = 22000))
-    employees.add(Sales(name = "Sazzad", age = 28, location = "Barisal", baseSalary = 22000))
-    employees.add(Sales(name = "Mahfuz", age = 28, location = "Barisal", baseSalary = 22000))
-    employees.add(Sales(name = "Sakib", age = 28, location = "kulna", baseSalary = 22000))
+    employees.add(Accountant(name = "Shihab", age = 25, location = "Sylhet", baseSalary = 30000))
+    employees.add(Accountant(name = "Saikat", age = 45, location = "Chittagong", baseSalary = 30000))
+    employees.add(Accountant(name = "Leon", age = 55, location = "Sylhet", baseSalary = 30000))
+    employees.add(Accountant(name = "Shafi", age = 55, location = "Chittagong", baseSalary = 30000))
+
+    employees.add(Manager(name = "Samir", age = 60, location = "feny", baseSalary = 55000))
+    employees.add(Manager(name = "samiya", age = 20, location = "Sylhet", baseSalary = 51000))
+    employees.add(Manager(name = "jannat", age = 50, location = "citagong", baseSalary = 45000))
+    employees.add(Manager(name = "Rabeya", age = 90, location = "feny", baseSalary = 54000))
+    employees.add(Manager(name = "Basori", age = 24, location = "Sylhet", baseSalary = 69000))
+
+    employees.add(Sales(name = "Rabbi", age = 29, location = "Feni", baseSalary = 22000))
+    employees.add(Sales(name = "Apple", age = 27, location = "naogaon", baseSalary = 22000))
+    employees.add(Sales(name = "Sazzad", age = 38, location = "Barisal", baseSalary = 22000))
+    employees.add(Sales(name = "Mahfuz", age = 48, location = "Chittagong", baseSalary = 22000))
+    employees.add(Sales(name = "Sakib", age = 53, location = "kulna", baseSalary = 22000))
 
     var marketingCount = 0
     var accountantCount = 0
@@ -56,9 +59,13 @@ for (employee in employees){
     val employAge = employee.age
     println("Age : $employAge")
 
+    val employLocation = employee.location
+    println("Location : $employLocation")
 
     val baseSalary = employee.salary()
     println("Base salary : $baseSalary")
+
+
 }
 
 
