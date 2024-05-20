b568fb08c43a5a71f15374656f217d1be33e9200
Almuheetu Shihab <almuheetu@gmail.com>
2024-05-20 12:54:59 +0600

    all problems checking and fixing

--- employeManejment/src/Main.kt
+++ employeManejment/src/Main.kt
@@ -3,30 +3,45 @@ fun main() {
 
 
 
-    val marketingEmployees =Marketing(name = "Karim", age = 30, location = "Dhaka", baseSalary = 15000)
-    employees.add(marketingEmployees)
+    val marketingEmployeesOne = Marketing(name = "Karim", age = 30, location = "Dhaka", baseSalary = 15000)
+    employees.add(marketingEmployeesOne)
+
+    val marketingEmployeesTwo = Marketing(name = "Rahim", age = 31, location = "Rajshahi", baseSalary = 15000)
+    employees.add(marketingEmployeesTwo)
+
+    val marketingEmployeesThree = Marketing(name = "jarin", age = 30, location = "Dhaka", baseSalary = 15000)
+    employees.add(marketingEmployeesThree)
 
 
-    employees.add(Marketing(name = "Rahim", age = 31, location = "Rajshahi", baseSalary = 15000))
-    employees.add(Marketing(name = "jarin", age = 32, location = "feny", baseSalary = 15000))
-    employees.add(Marketing(name = "harim", age = 33, location = "citagong", baseSalary = 15000))
+    val accountingEmployeesOne = Accountant(name = "Shihab", age = 30, location = "Rajshahi", baseSalary = 15000)
+    employees.add(accountingEmployeesOne)
 
-    employees.add(Accountant(name = "Shihab", age = 25, location = "Sylhet", baseSalary = 30000))
-    employees.add(Accountant(name = "Saikat", age = 45, location = "Chittagong", baseSalary = 30000))
-    employees.add(Accountant(name = "Leon", age = 55, location = "Sylhet", baseSalary = 30000))
-    employees.add(Accountant(name = "Shafi", age = 55, location = "Chittagong", baseSalary = 30000))
+    val accountingEmployeesTwo = Accountant(name = "Saikat", age = 30, location = "Dhaka", baseSalary = 15000)
+    employees.add(accountingEmployeesTwo)
 
-    employees.add(Manager(name = "Samir", age = 60, location = "feny", baseSalary = 55000))
-    employees.add(Manager(name = "samiya", age = 20, location = "Sylhet", baseSalary = 51000))
-    employees.add(Manager(name = "jannat", age = 50, location = "citagong", baseSalary = 45000))
-    employees.add(Manager(name = "Rabeya", age = 90, location = "feny", baseSalary = 54000))
-    employees.add(Manager(name = "Nusrat", age = 24, location = "Rajshahi", baseSalary = 69000))
+    val accountingEmployeesThree = Accountant(name = "Leon", age = 34, location = "Mohadevpur", baseSalary = 15000)
+    employees.add(accountingEmployeesThree)
 
-    employees.add(Sales(name = "Rabbi", age = 29, location = "Feni", baseSalary = 22000))
-    employees.add(Sales(name = "Apple", age = 27, location = "naogaon", baseSalary = 22000))
-    employees.add(Sales(name = "Sazzad", age = 38, location = "Barisal", baseSalary = 22000))
-    employees.add(Sales(name = "Mahfuz", age = 48, location = "Chittagong", baseSalary = 22000))
-    employees.add(Sales(name = "Sakib", age = 53, location = "kulna", baseSalary = 22000))
+
+    val managerEmployeesOne = Manager(name = "Samir", age = 60, location = "feny", baseSalary = 50000)
+    employees.add(managerEmployeesOne)
+
+    val managerEmployeesTwo = Manager(name = "samiya", age = 20, location = "Sylhet", baseSalary = 60000)
+    employees.add(managerEmployeesTwo)
+
+    val managerEmployeesThree = Manager(name = "Nusrat", age = 22, location = "Rajshahi", baseSalary = 70000)
+    employees.add(managerEmployeesThree)
+
+
+    val salesEmployeesOne = Sales(name = "Rabbi", age = 22, location = "Rajshahi", baseSalary = 22000)
+    employees.add(salesEmployeesOne)
+
+    val salesEmployeesTwo = Sales(name = "Sakib", age = 24, location = "Dhaka", baseSalary = 25000)
+    employees.add(salesEmployeesTwo)
+
+    val salesEmployeesThree = Sales(name = "Mahfuz", age = 22, location = "Rajshahi", baseSalary = 21000)
+    employees.add(salesEmployeesThree)
+
 
     var marketingCount = 0
     var accountantCount = 0
@@ -51,30 +66,36 @@ fun main() {
     println("Manager : $managerCount")
     println("Sales : $salesCount")
 
-for (employee in employees){
+    for (employee in employees) {
 
-    val employName = employee.name
-    println("Name : $employName")
+//        val marketingEmploy = marketingCount
+//        println("marketingEmploy : $marketingEmploy")
 
-    val employAge = employee.age
-    println("Age : $employAge")
+        val employName = employee.name
+        println("Name : $employName")
 
-    val employLocation = employee.location
-    println("Location : $employLocation")
+        val employAge = employee.age
+        println("Age : $employAge")
 
-    val baseSalary = employee.salary()
-    println("Base salary : $baseSalary")
+        val employLocation = employee.location
+        println("Location : $employLocation")
 
-    val bonusSalary = (employee as EmployBonus).calculateBonus(baseSalary)
-    println("BonusSalary : $bonusSalary")
+        val baseSalary = employee.salary()
+        println("BaseSalary : $baseSalary")
 
-    val mdBonus = BonusfromMd(employee).bonus()
-    println("BonusfromMd : $mdBonus")
+        val bonusSalary = (employee as EmployBonus).calculateBonus(baseSalary)
+        println("BonusSalary : $bonusSalary")
 
-    val totalLeave = (employee as EmployLeave).leaveCalculator()
-    println("TotalLeave : $totalLeave")
+        val mdBonus = BonusfromMd(employee).bonus()
+        println("BonusfromMd : $mdBonus")
 
-}
+        val totalLeave = (employee as EmployLeave).leaveCalculator()
+        println("TotalLeave : $totalLeave")
+
+        val totalSalary = baseSalary + bonusSalary + mdBonus
+        println("TotalSalary : $totalSalary")
+
+    }
 
 
 }
